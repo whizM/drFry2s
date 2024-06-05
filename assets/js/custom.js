07e762c0ceefe88c30c546f00d6f55ac8b411772
techguru0411 <techguru0411@gmail.com>
2024-06-05 10:47:11 +0900

    Dry Fry static pages

---
+++ assets/js/custom.js
@@ -0,0 +1,27 @@
+ScrollReveal().reveal('.fade_left', { 
+  duration: 1600, 
+  origin: 'left', 
+  distance: '50px',
+  // reset: true   
+});
+
+ScrollReveal().reveal('.fade_right', { 
+  duration: 1600, 
+  origin: 'right', 
+  distance: '50px',
+  // reset: true   
+});
+
+ScrollReveal().reveal('.fade_bottom_ttl', { 
+  duration: 1500, 
+  origin: 'bottom', 
+  distance: '50px',
+  // reset: true   
+});
+
+ScrollReveal().reveal('.fade_bottom_content', { 
+  duration: 2500, 
+  origin: 'bottom', 
+  distance: '50px',
+  // reset: true   
+});
